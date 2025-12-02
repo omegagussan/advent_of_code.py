e5510d06ce4567a8cdf35f827deb9321c5592f16
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2025-12-02 09:52:07 +0100

    part2

--- src/2025/days/day2/main.py
+++ src/2025/days/day2/main.py
@@ -13,10 +13,41 @@ def part1():
 					sum += i
 	return sum
 
+def part2():
+	sum = 0
+	with open("input.txt") as input_file:
+		pairs = input_file.read().strip().split(',')
+		for pair in pairs:
+			a, b = pair.split('-')
+			start = int(a, 10)
+			end = int(b, 10)
+			for i in range(start, end + 1):
+				if is_repeated(str(i)):
+					sum += i
+	return sum
+
+def is_repeated(s: str) -> bool:
+	for i in range(2, len(s) + 1):
+		if len(s) % i != 0:
+			continue
+		ps = split_in_equal_parts(s, i)
+		found = True
+		for p in ps[1:]:
+			if p != ps[0]:
+				found = False
+		if found:
+			return True
+	return False
+
 def is_repeated_twice(s: str) -> bool:
 	first_half = s[:len(s)//2]
 	second_half = s[len(s)//2:]
 	return first_half == second_half
 
+def split_in_equal_parts(s: str, n: int) -> list[str]:
+	part_length = len(s) // n
+	return [s[i*part_length:(i+1)*part_length] for i in range(n)]
+
 if __name__ == "__main__":
 	print(part1())
+	print(part2())
