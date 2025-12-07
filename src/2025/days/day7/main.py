4d9d85ed4aa81e2c2674f06fe18f9256d86c3a93
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2025-12-07 10:42:13 +0100

    wip

--- src/2025/days/day7/main.py
+++ src/2025/days/day7/main.py
@@ -12,10 +12,8 @@ def part1():
             for b in beam_heads:
                 v = row[b]
                 if v == '^':
-                    old = len(next_beam_heads)
                     next_beam_heads.add(b-1)
                     next_beam_heads.add(b+1)
-                    #if len(next_beam_heads) > old:
                     tot += 1
                 else:
                     next_beam_heads.add(b)
@@ -25,5 +23,28 @@ def part1():
 
     return tot
 
+def part2():
+    with open("sample.txt") as input_file:
+        rows = input_file.read().strip().split('\n')
+        beam_heads = set()
+        for j, v in enumerate(rows[0]):
+            if v == 'S':
+                beam_heads.add((j,))
+        print(f"initial beam heads: {beam_heads}")
+        for i, row in enumerate(rows):
+            curr_beam_heads = beam_heads.copy()
+            next_beam_heads = set()
+            for curr in curr_beam_heads:
+                b = curr[-1]
+                v = row[b]
+                if v == '^':
+                    next_beam_heads.add(curr + (b-1,))
+                    next_beam_heads.add(curr + (b+1,))
+                else:
+                    next_beam_heads.add(curr + (b,))
+            beam_heads = next_beam_heads
+    return len(beam_heads)
+
 if __name__ == "__main__":
-    print(part1())
+    #print(part1())
+    print(part2())
