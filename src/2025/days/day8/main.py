e882699a58698910071b7eacc4abf3a81c15fd68
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2025-12-08 17:52:55 +0100

    tidy

--- src/2025/days/day8/main.py
+++ src/2025/days/day8/main.py
@@ -3,88 +3,63 @@ from collections import Counter
 def distance(p1, p2):
     return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2 + (p1[2]-p2[2])**2
 
-def part1():
-    comps = 1000
+def read_points(filename="input.txt"):
+    with open(filename) as f:
+        return [tuple(map(int, line.split(','))) for line in f]
 
-    # Read points
-    with open("input.txt") as f:
-        points = [tuple(map(int, line.split(','))) for line in f]
-
-    # Generate all unique edges with distances
-    dists = [(distance(points[i], points[j]), i, j)
-             for i in range(len(points)) for j in range(i+1, len(points))]
-    dists.sort(key=lambda x: x[0])
-
-    # Union-find setup
-    parent = list(range(len(points)))
-    size = [1]*len(points)
-
-    def find(x):
-        while parent[x] != x:
-            parent[x] = parent[parent[x]]
-            x = parent[x]
-        return x
-
-    def union(a, b):
-        ra, rb = find(a), find(b)
-        if ra == rb:
-            return
-        if size[ra] < size[rb]:
-            ra, rb = rb, ra
-        parent[rb] = ra
-        size[ra] += size[rb]
-
-    # Process the comps shortest edges
-    for _, i, j in dists[:comps]:
-        union(i, j)
-
-    # Count component sizes
-    comp_sizes = Counter(find(i) for i in range(len(points)))
-    largest = sorted(comp_sizes.values(), reverse=True)
-
-    return largest[0]*largest[1]*largest[2]
-
-def part2():
-    # Read points
-    with open("input.txt") as f:
-        points = [tuple(map(int, line.split(','))) for line in f]
-
+def generate_edges(points):
     n = len(points)
-
-    # Generate all unique edges with distances
-    dists = [(distance(points[i], points[j]), i, j)
+    edges = [(distance(points[i], points[j]), i, j)
              for i in range(n) for j in range(i+1, n)]
-    dists.sort(key=lambda x: x[0])
+    edges.sort(key=lambda x: x[0])
+    return edges
 
-    # Union-find setup
-    parent = list(range(n))
-    size = [1]*n
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.size = [1]*n
 
-    def find(x):
-        while parent[x] != x:
-            parent[x] = parent[parent[x]]
-            x = parent[x]
+    def find(self, x):
+        while self.parent[x] != x:
+            self.parent[x] = self.parent[self.parent[x]]
+            x = self.parent[x]
         return x
 
-    def union(a, b):
-        ra, rb = find(a), find(b)
+    def union(self, a, b):
+        ra, rb = self.find(a), self.find(b)
         if ra == rb:
             return False
-        if size[ra] < size[rb]:
+        if self.size[ra] < self.size[rb]:
             ra, rb = rb, ra
-        parent[rb] = ra
-        size[ra] += size[rb]
-        return True  # merged
+        self.parent[rb] = ra
+        self.size[ra] += self.size[rb]
+        return True
 
-    # Process edges until all connected
-    for _, i, j in dists:
-        if union(i, j):
-            if size[find(i)] == n:
-                # i and j are the last two junctions to connect the full circuit
+def part1(points, comps=1000):
+    edges = generate_edges(points)
+    uf = UnionFind(len(points))
+
+    for _, i, j in edges[:comps]:
+        uf.union(i, j)
+
+    from collections import Counter
+    comp_sizes = Counter(uf.find(i) for i in range(len(points)))
+    largest = sorted(comp_sizes.values(), reverse=True)
+    return largest[0]*largest[1]*largest[2]
+
+def part2(points):
+    edges = generate_edges(points)
+    n = len(points)
+    uf = UnionFind(n)
+
+    for _, i, j in edges:
+        if uf.union(i, j):
+            if uf.size[uf.find(i)] == n:
                 return points[i][0] * points[j][0]
 
-    return -1  # should not reach here
+    return -1  # should not happen
 
 if __name__ == "__main__":
-    #print(part1())
-    print(part2())
+    points = read_points("input.txt")
+    print("Part 1:", part1(points))
+    print("Part 2:", part2(points))
