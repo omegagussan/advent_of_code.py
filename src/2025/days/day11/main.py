2bdd2a9799866109a9ed6b71d3e267ca7da38ce2
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2025-12-11 12:06:10 +0100

    part2

--- src/2025/days/day11/main.py
+++ src/2025/days/day11/main.py
@@ -1,4 +1,4 @@
-
+import functools
 
 def part1():
 	with open("input.txt") as input_file:
@@ -13,18 +13,35 @@ def part1():
 
 	return count_paths(links, "you", "out")
 
+def part2():
+	with open("input.txt") as input_file:
+		links = dict()
+		rows = input_file.read().strip().split('\n')
+		#aaa: you hhh => aaa: [you, hhh] in the links
+		for row in rows:
+			a, b = row.split(':')
+			b_list = [v.strip() for v in b.split(' ') if v.strip() != '']
+			links[a.strip()] = b_list
+		print(links)
+	#needs to pass via "dac" and "fft" nodes in order
+	first = count_paths(links, "svr", "fft")
+	print(f"svr -> fft: {first}")
+	second = count_paths(links, "fft", "dac")
+	print(f"fft -> dac: {second}")
+	third = count_paths(links, "dac", "out")
+	print(f"dac -> out: {third}")
+	return first * second * third
 
-def count_paths(links: dict[str, list[str]], start: str, end: str, visited=None) -> int:
-	if visited is None:
-		visited = set()
-	if start == end:
-		return 1
-	visited.add(start)
-	total = 0
-	for neighbor in links.get(start, []):
-		if neighbor not in visited:
-			total += count_paths(links, neighbor, end, visited.copy())
-	return total
+
+def count_paths(links, start, end):
+	@functools.lru_cache(None)
+	def dfs(node):
+		if node == end:
+			return 1
+		return sum(dfs(n) for n in links.get(node, []))
+
+	return dfs(start)
 
 if __name__ == "__main__":
 	print(part1())
+	print(part2())
